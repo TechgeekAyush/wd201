3133aae31f9040bd09cf247c9c57d10002931249
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2024-09-30 12:11:49 +0530

    L5 milestone

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { Op } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -14,35 +15,71 @@ module.exports = (sequelize, DataTypes) => {
     }
     static async showList() {
       console.log("My Todo list \n");
-
+      const overdueTodos = await Todo.overdue();
+      const dueTodayTodos = await Todo.dueToday();
+      const dueLaterTodos = await Todo.dueLater();
       console.log("Overdue");
       // FILL IN HERE
+      overdueTodos.forEach((todo, index) => {
+        console.log(`${index + 1}. [${todo.completed ? 'x' : ' '}] ${todo.title} ${todo.dueDate}`);
+      });
       console.log("\n");
 
       console.log("Due Today");
       // FILL IN HERE
+      dueTodayTodos.forEach((todo, index) => {
+        console.log(`${index + 1}. [${todo.completed ? 'x' : ' '}] ${todo.title}`);
+      });
       console.log("\n");
 
       console.log("Due Later");
       // FILL IN HERE
+      dueLaterTodos.forEach((todo, index) => {
+        console.log(`${index + 1}. [${todo.completed ? 'x' : ' '}] ${todo.title} ${todo.dueDate}`);
+      });
     }
 
     static async overdue() {
       // FILL IN HERE TO RETURN OVERDUE ITEMS
-      
+      const today = new Date().toISOString().split('T')[0];
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: today
+          }
+        }
+      });
     }
 
     static async dueToday() {
       // FILL IN HERE TO RETURN ITEMS DUE tODAY
+      const today = new Date().toISOString().split('T')[0];
+      return await Todo.findAll({
+        where: {
+          dueDate: today
+        }
+      });
     }
 
     static async dueLater() {
       // FILL IN HERE TO RETURN ITEMS DUE LATER
+      const today = new Date().toISOString().split('T')[0];
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: today
+          }
+        }
+      });
     }
 
     static async markAsComplete(id) {
       // FILL IN HERE TO MARK AN ITEM AS COMPLETE
-
+      return await Todo.update({ completed: true }, {
+        where: {
+          id: id
+        }
+      });
     }
 
     displayableString() {
